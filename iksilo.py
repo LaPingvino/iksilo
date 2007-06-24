5e77c7b1eab6cb80cbcc0d10b85ffb288cf4a69a
Joop Kiefte <ikojba@gmail.com>
2007-06-24 19:54:27 +0000

    Mi aldonis mian nomon kaj etan mencion pri GPL

--- iksilo.py
+++ iksilo.py
@@ -1,5 +1,10 @@
-## Cxi tie mi difinas kiujn Unikodajxojn la programo devos uzi.
-## La strukturo uzata estas vortara.
+# Licenco: GPL
+# Ankoraŭ aldonenda al la kodo
+# Projektanoj:
+# Ĉefprogramisto: LaPingvino
+
+# Cxi tie mi difinas kiujn Unikodajxojn la programo devos uzi.
+# La strukturo uzata estas vortara.
 espliteroj =\
  {'C': u"\u0108", 'G': u"\u011C", 'H': u"\u0124", 'J': u"\u0134", 'S': u"\u015C", 'U': u"\u016C",\
  'c': u"\u0109", 'g': u"\u011D", 'h': u"\u0125", 'j': u"\u0135", 's': u"\u015D", 'u': u"\u016D"}
