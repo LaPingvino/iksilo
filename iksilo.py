50f120562d071dce050c36417851e8f7c3236b0a
Joop Kiefte <ikojba@gmail.com>
2007-06-25 12:43:45 +0000

    Aldono de transskribo

--- iksilo.py
+++ iksilo.py
@@ -1,10 +1,18 @@
 # Licenco: GPL
-# Ankoraŭ aldonenda al la kodo
+# Ankoraux aldonenda al la kodo
 # Projektanoj:
-# Ĉefprogramisto: LaPingvino
+# Cxefprogramisto: LaPingvino
 
 # Cxi tie mi difinas kiujn Unikodajxojn la programo devos uzi.
 # La strukturo uzata estas vortara.
 espliteroj =\
  {'C': u"\u0108", 'G': u"\u011C", 'H': u"\u0124", 'J': u"\u0134", 'S': u"\u015C", 'U': u"\u016C",\
  'c': u"\u0109", 'g': u"\u011D", 'h': u"\u0125", 'j': u"\u0135", 's': u"\u015D", 'u': u"\u016D"}
+
+# Cxi tie venas la transskriba parto
+def transskribu(enigo):
+    eligo = enigo
+    for deveno, aliro in espliteroj.items():
+        eligo = eligo.replace(deveno+"x", aliro)
+        eligo = eligo.replace(deveno+"X", aliro)
+    
